9a97053089728cfa479eea9b6b03112eb9d2c974
Christian <ps5christian@hotmail.com>
2024-04-15 08:02:18 +0200

    Filtered the hourly data objects
    New data objects with pollen data to the coresponding timestamp

--- assets/js/Components/pollen.js
+++ assets/js/Components/pollen.js
@@ -20,18 +20,6 @@ const getUserLocationName = async (lat, long) => {
   recivedLocationName(userLocationData);
 };
 
-export const getPollenData = async (lat, long) => {
-  console.log("Pollen!!");
-  const endpoint = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${long}&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&timezone=Europe%2FBerlin&forecast_days=1`;
-  const pollenData = await myFetchData(endpoint);
-  //   console.log(pollenData);
-  recivedPollenData(pollenData);
-};
-
-const recivedPollenData = (pollenData) => {
-  console.log(pollenData);
-};
-
 const recivedPosition = (position) => {
   //   console.log("latitude:" + position.coords.latitude);
   //   console.log("longitude:" + position.coords.longitude);
@@ -47,4 +35,43 @@ const showPositionError = (error) => {
 const recivedLocationName = (locationName) => {
   console.log(locationName);
   console.log(locationName.address.town);
+};
+
+export const getPollenData = async (lat, long) => {
+  console.log("Pollen!!");
+  const endpoint = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${long}&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&timezone=Europe%2FBerlin&forecast_days=1`;
+  const pollenData = await myFetchData(endpoint);
+  //   console.log(pollenData);
+  recivedPollenData(pollenData);
+};
+
+const recivedPollenData = (pollenData) => {
+  let viewData = [];
+  viewData.push(pollenData);
+
+  //   console.log(viewData);
+
+  let timeStamps = pollenData.hourly.time;
+
+  let hourData = [];
+
+  timeStamps.map((timestamp, index) => {
+    let hourDataObjects = {};
+    //Creating new data objects for each hourly pollen
+    //Each data object get the pollen type and the corresponding timestamp from the 24 hour array
+    hourDataObjects.time = timestamp;
+    hourDataObjects.alder_pollen = pollenData.hourly.alder_pollen[index];
+    hourDataObjects.birch_pollen = pollenData.hourly.birch_pollen[index];
+    hourDataObjects.grass_pollen = pollenData.hourly.grass_pollen[index];
+    hourDataObjects.mugwort_pollen = pollenData.hourly.mugwort_pollen[index];
+    hourDataObjects.olive_pollen = pollenData.hourly.olive_pollen[index];
+    hourDataObjects.ragweed_pollen = pollenData.hourly.ragweed_pollen[index];
+
+    hourData.push(hourDataObjects);
+  });
+
+  //Build current pollen
+  buildCurrentPollen(viewData);
+  //Build hourly pollen
+  buildHourlyPollen(hourData);
 };
